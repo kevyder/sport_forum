900838c219e4ed4da58a86b8d171ad3e19e8cc85
Kevin <kyrodriguez97@gmail.com>
2020-07-01 21:38:02 -0500

    create new post action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,33 @@
 
 class PostsController < ApplicationController
   def index; end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :description)
+  end
 end
