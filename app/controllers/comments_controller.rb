ae6a82dd63b591c036c232a9b9f5c2f8678b3de7
Kevin <kyrodriguez97@gmail.com>
2020-07-04 17:46:19 -0500

    Edit and delete comments action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,10 @@
+# frozen_string_literal: true
+
 class CommentsController < ApplicationController
+  before_action :find_post
+  before_action :find_comment, exclude: [:create]
+
   def create
-    @post = Post.find(params[:post_id])
     @comment = @post.comments.create(comment_params)
     @comment.user_id = current_user.id
 
@@ -11,10 +15,32 @@ class CommentsController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @comment.update(comment_params)
+      redirect_to post_path(@post)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @comment.destroy
+    redirect_to post_path(@post)
+  end
+
   private
 
   def comment_params
     params.require(:comment).permit(:content)
   end
 
+  def find_post
+    @post = Post.find(params[:post_id])
+  end
+
+  def find_comment
+    @comment = @post.comments.find(params[:id])
+  end
 end
